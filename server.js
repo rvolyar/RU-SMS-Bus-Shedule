08595bef5aafdb5b19e5beb07ba5dc8b9e11e6b5
Ruslan Volyar <prizerebelcrap101@gmail.com>
2017-10-17 17:20:33 +0000

    added basic twilio functionality

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var twilio = require("twilio");
 var http = require("http");
 var express = require("express");
+var bodyParser = require("body-parser");
 require('dotenv').config();
 var sid = process.env.SID;
 var token = process.env.TOKEN;
-console.log("hi");
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+console.log("hi! Your app is running!");
 //console.log(sid +"/n"+  token);
+var app = express();
+app.use(bodyParser());
+app.post('/message', function(req, res) {
+  const twiml = new MessagingResponse();
+ 
+  var msgBody = req.body.Body;
+  console.log("Message was " + msgBody);
+  twiml.message('Message Recieved! Message was:' + msgBody );
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(process.env.PORT, process.env.IP);
