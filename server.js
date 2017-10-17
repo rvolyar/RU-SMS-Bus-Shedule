96c85c520bda0d66c6b9719e689f7e5bf5d1bc53
Ruslan Volyar <prizerebelcrap101@gmail.com>
2017-10-17 06:34:56 +0000

    added env for auth

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 var twilio = require("twilio");
+var http = require("http");
+var express = require("express");
+require('dotenv').config();
+var sid = process.env.SID;
+var token = process.env.TOKEN;
+console.log(sid +"/n"+  token);
