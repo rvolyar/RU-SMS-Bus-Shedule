2afcd26486d51816c3fe067525ad7e010d188e93
Ruslan Volyar <prizerebelcrap101@gmail.com>
2017-10-25 18:59:24 +0000

    added nextbusjs api

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ require('dotenv').config();
 var sid = process.env.SID;
 var token = process.env.TOKEN;
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
+var request = require("request");
+var rutgers = require('nextbusjs').client();
+function busRequest(busName){
+request('https://rumobile.rutgers.edu/1/rutgersrouteconfig.txt', function(error, response, body){
+   console.log('error:', error); // Print the error if one occurred and handle it
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      //console.log(body);
+      rutgers.setAgencyCache(JSON.parse(body),'rutgers');
+      rutgers.routePredict(busName, null, function (err, data){
+        console.log(err);
+        console.log(data);
+        return data;
+      }, 'minutes');
+});
+}
 
 console.log("hi! Your app is running!");
 //console.log(sid +"/n"+  token);
@@ -21,5 +35,6 @@ app.post('/message', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
+
 
 http.createServer(app).listen(process.env.PORT, process.env.IP);
